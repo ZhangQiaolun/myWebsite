e74af215fea85e9631008f8de211309f7238ca3f
zhql0907 <zhql0907@outlook.com>
2016-10-15 09:16:12 +0800

    add blogtastic to the repo

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# myWebsite
+# myWebsiteThere are 8 websites following the book *Practical PHP and MySQL Building Eight Dynamic Web Applications*
---
+++ blogtastic/config.php
@@ -0,0 +1,14 @@
+<?php
+
+$dbhost = "localhost";
+$dbuser = "zhang";
+$dbpassword = "19960907zql";
+$dbdatabase = "blogtastic";
+
+$config_blogname = "Funny old world";
+
+$config_author = "Jono Bacon";
+
+$config_basedir = "http://127.0.0.1/~zhang/blogtastic/";
+
+?>
